35b8970b28d991dd3ca2508d47532b0950d74f94
BeFlama <florencia.n.benitez@hotmail.com>
2023-02-22 02:57:21 -0300

    dora la calculadora

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
-const displayValorAnterior = document.getElementById(`prevalor`)
-const displayValorActual = document.getElementById(`actualvalor`)
-const botonNros = document.querySelectorAll(`.nro`)
-const botonOper = document.querySelectorAll(`.oper`)
+const displayValorAnterior = document.getElementById('prevalor')
+const displayValorActual = document.getElementById('actualvalor')
+const botonNros = document.querySelectorAll('.nro')
+const botonOper = document.querySelectorAll('.oper')
 
-const calculadora = new Calculadora();
+/* const calculadora = new Calculadora();
 
-console.log(calculadora.sumar(2,3))
+console.log(calculadora.sumar(5,3))
+console.log(calculadora.restar(5,3))
+console.log(calculadora.multiplicar(5,3))
+console.log(calculadora.dividir(5,3))
+ */
+
+const display = new Display(displayValorAnterior, displayValorActual)
+
+botonNros.forEach(boton => {
+    boton.addEventListener('click', ()=>{
+        display.agregarNro(boton.innerHTML)
+    })
+})
