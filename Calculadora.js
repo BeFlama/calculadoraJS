bf2d995dec736f4b0f5a3c2981c2296a5513d8a4
BeFlama <florencia.n.benitez@hotmail.com>
2023-03-30 00:48:11 -0300

    calcula2

--- Calculadora.js
+++ Calculadora.js
@@ -1,8 +1,8 @@
+/* un file con class como este de js se inicia con mayusucla en los archivos */
 class Calculadora {
     sumar(num1, num2){
         return num1 + num2;
     }
-/* un file con class como este de js se inicia con mayusucla en los archivos */
     restar(num1, num2){
         return num1 - num2;
     }
--- index.js
+++ index.js
@@ -3,18 +3,14 @@ const displayValorActual = document.getElementById('actualvalor')
 const botonNros = document.querySelectorAll('.nro')
 const botonOper = document.querySelectorAll('.oper')
 
-/* const calculadora = new Calculadora();
-
-console.log(calculadora.sumar(5,3))
-console.log(calculadora.restar(5,3))
-console.log(calculadora.multiplicar(5,3))
-console.log(calculadora.dividir(5,3))
- */
-
 const display = new Display(displayValorAnterior, displayValorActual)
 
 botonNros.forEach(boton => {
     boton.addEventListener('click', ()=>{
         display.agregarNro(boton.innerHTML)
     })
+})
+
+botonOper.forEach(boton => {
+    boton.addEventListener('click', ()=> display.computar(boton.value))
 })
